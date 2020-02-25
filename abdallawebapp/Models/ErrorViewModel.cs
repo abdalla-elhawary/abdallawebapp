820acd4dd0731b1d2aa9ca6eee988fa25601aa4a
Abdalla Fawaz El-Hawary <abdalla_fawaz@hotmail.com>
2020-02-25 06:30:28 +0000

    commit

---
+++ abdallawebapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace abdallawebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
